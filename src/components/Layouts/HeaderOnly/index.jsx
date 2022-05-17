6fabb9477c81f835f203a51ac301375907d47d19
quangvinht <96629186+quangvinht@users.noreply.github.com>
2022-05-17 14:23:04 +0700

    Bai 63 - change somthing

--- src/components/Layouts/HeaderOnly/index.jsx
+++ src/components/Layouts/HeaderOnly/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '~/components/Layouts/DefaultLayout/Header';
+import Header from '~/components/Layouts/components/Header';
 
 function DefaultLayout({ children }) {
     return (
