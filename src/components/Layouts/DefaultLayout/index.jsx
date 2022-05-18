3b8ce90e6e0561076ca37e3a311d716da71d83b5
quangvinht <96629186+quangvinht@users.noreply.github.com>
2022-05-18 17:11:33 +0700

    Bai 64 : Dựng khung Layout mặc định

--- src/components/Layouts/DefaultLayout/index.jsx
+++ src/components/Layouts/DefaultLayout/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayout.module.scss';
 import Header from '~/components/Layouts/components/Header';
 import Sidebar from '~/components/Layouts/DefaultLayout/Sidebar';
 
+const cx = classNames.bind(styles);
+
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
